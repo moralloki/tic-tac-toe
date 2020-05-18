0f3bda4f10420c33cf744acd572cc13a74a3e3e4
Christopher Williams <Chris.Williams@Tideworks.com>
2020-05-18 10:15:37 -0700

    Fixed it so it works with the new methods

--- ttt.py
+++ ttt.py
@@ -2,45 +2,82 @@
 
 
 from TicTacToe import TicTacToe
+from copy import deepcopy
+from random import choice as random_choice
 
 
-def select_square_or_quit() -> int:
+def computer_move(board):
+    center = [5]
+    corners = [1, 3, 7, 9]
+    edges = [2, 4, 6, 8]
+    available_moves = board.get_available_locations()
+    print("Computer is choosing from: {}".format(available_moves))
+
+    # Can I win on my next move?
+    for move in available_moves:
+        # Create a board copy
+        board_copy = deepcopy(board)
+        board_copy.update_board(move)
+        if board_copy.winner():
+            return move
+
+    # Can I block against a win with my next move?
+    for move in available_moves:
+        # Create a board copy
+        board_copy = deepcopy(board)
+        # Switch to the next player
+        board_copy.turn_number += 1
+        board_copy.update_board(move)
+        if board_copy.winner():
+            return move
+
+    # Run through corners, center, then edges for next available move
+    for l in corners, center, edges:
+        choices = list(set(l).intersection(available_moves))
+        if choices:
+            choice = random_choice(choices)
+            break
+    # BUG: This doesn't always print
+    print("Computer choose {}. Your move...".format(choice))
+    return choice
+
+
+def get_player_move(board) -> int:
     """Get user input for next square to select
 
     Raises:
         ValueError: When input is not between 1 through 9 (inclusive)
-        SystemExit: Raised when 'Q' or 'q' is input
 
     Returns:
         int -- 1-9
     """
-    selection = input("Select an unoccupied square (1-9, or 'q' to quit): ")
+    selection = int(
+        input("Select an unoccupied square {}: ".format(board.get_available_locations())))
 
-    if selection.lower() == 'q':
-        raise SystemExit
     if not 1 <= int(selection) <= 9:
         raise ValueError
-    return int(selection)
+    return selection
 
 
 def main():
     # Global vars
     game_over = False
-    markers = ['A', 'B']
+    is_human_turn = True
 
-    board = TicTacToe(markers)
+    # Start with Human player as 'X' (default)
+    board = TicTacToe()
     while not game_over:
         board.draw()
-        try:
-            selection = select_square_or_quit()
-            board.update_board(selection)
-            # selection, markers[board.get_turn() % len(markers)])
-        except ValueError:
-            print("Sorry, please select an unoccupied square (1-9)")
-            continue
-        except SystemExit:
-            print("Thanks for playing...")
-            exit(0)
+        if is_human_turn:
+            try:
+                selection = get_player_move(board)
+                board.update_board(selection)
+            except ValueError:
+                print("Sorry, please select an unoccupied square {}".format(
+                    board.available_locations))
+                continue
+        else:
+            board.update_board(computer_move(board))
         if board.winner():
             print(f"{board.winner()} is the winner!")
             game_over = True
@@ -49,6 +86,8 @@ def main():
             print("It's a draw!")
             game_over = True
             continue
+        # Flip our player
+        is_human_turn = not is_human_turn
 
     board.draw()
 
